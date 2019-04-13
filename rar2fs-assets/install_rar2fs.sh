4c9885f778f2a1601486f6cbba391062cb2c42d4
eccoes <49564549+eccoes@users.noreply.github.com>
2019-04-13 11:41:29 +0200

    Update install_rar2fs.sh

--- rar2fs-assets/install_rar2fs.sh
+++ rar2fs-assets/install_rar2fs.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
+
 cd /tmp
 git clone https://github.com/hasse69/rar2fs.git
 cd rar2fs
-wget http://www.rarlab.com/rar/unrarsrc-5.4.6.tar.gz
-tar -zxvf unrarsrc-5.4.6.tar.gz
-
+wget http://www.rarlab.com/rar/unrarsrc-5.7.3.tar.gz
+tar -xvzf unrarsrc-5.7.3.tar.gz
 cd unrar
 make lib
 make install-lib
 cd ..
-
 autoreconf -f -i
 ./configure && make
 make install
+cd /
