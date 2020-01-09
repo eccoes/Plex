a518819ecac8d425aa749e7e0e062e7c8c9cebb6
eccoes <49564549+eccoes@users.noreply.github.com>
2020-01-09 15:20:42 +0100

    Update install_rar2fs.sh

--- rar2fs-assets/install_rar2fs.sh
+++ rar2fs-assets/install_rar2fs.sh
@@ -3,7 +3,7 @@
 cd /tmp
 git clone https://github.com/hasse69/rar2fs.git
 cd rar2fs
-wget https://www.rarlab.com/rar/unrarsrc-5.8.3.tar.gz
+wget https://www.rarlab.com/rar/unrarsrc-5.8.5.tar.gz
 tar -xvzf unrarsrc-5.8.3.tar.gz
 cd unrar
 make lib
